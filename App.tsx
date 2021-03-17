7d40f66fb6aaf8f96f95dee707e7ccf1438b9565
ElbertRibeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2021-03-17 20:46:33 -0300

    update

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  //const Test: any = alert('teste');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+         <Text>O que vou fazer hoje?</Text>
+         <TextInput />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
