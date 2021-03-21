b02926d87e59ce453ff95da5d7aa5ed9493189ce
ElbertRibeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2021-03-21 19:17:29 -0300

    update styles

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import Home from './src/pages';
 
 export default function App() {
   return (
     <View>
+      <StatusBar style="auto"/>
       <Home />
     </View>
   );
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
+import styles from './styles';
 
-const Task = (props: any) => {
-
+export default function Task(props: any) {
   return (
     <View style={styles.item}>
       <View style={styles.itemLeft}>
@@ -11,42 +11,5 @@ const Task = (props: any) => {
       </View>
       <View style={styles.circular}></View>
     </View>
-  )
+  );
 }
-
-const styles = StyleSheet.create({
-  item: {
-    backgroundColor: '#FFF',
-    padding: 15,
-    borderRadius: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginBottom: 20,
-  },
-  itemLeft: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    flexWrap: 'wrap'
-  },
-  square: {
-    width: 24,
-    height: 24,
-    backgroundColor: '#55BCF6',
-    opacity: 0.4,
-    borderRadius: 5,
-    marginRight: 15,
-  },
-  itemText: {
-    maxWidth: '80%',
-  },
-  circular: {
-    width: 12,
-    height: 12,
-    borderColor: '#55BCF6',
-    borderWidth: 2,
-    borderRadius: 5,
-  },
-});
-
-export default Task;
